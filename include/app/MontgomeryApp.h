176ccd277b1311d61cdbb01a8ef30760c692a85d
Wiktor Wilczewski <wiktorczewski@gmail.com>
2025-04-08 21:09:02 +0200

    create project

---
+++ include/app/MontgomeryApp.h
@@ -0,0 +1,16 @@
+//
+// Created by wiktor on 08.04.2025.
+//
+
+#ifndef MONTGOMERYAPP_H
+#define MONTGOMERYAPP_H
+
+
+
+class MontgomeryApp {
+
+};
+
+
+
+#endif //MONTGOMERYAPP_H
---
+++ src/app/MontgomeryApp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wiktor on 08.04.2025.
+//
+
+#include "MontgomeryApp.h"
